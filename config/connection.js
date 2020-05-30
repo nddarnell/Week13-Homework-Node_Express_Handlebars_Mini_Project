578905643763b69eaedf0810e7a890d379c7ec3d
Nathan Darnell <darnell.nathaniel95@gmail.com>
2020-05-29 18:00:42 -0700

    added required files from activity 17 that were provided for us.

--- config/connection.js
+++ config/connection.js
@@ -1,2 +1,24 @@
 // connection to database goes here
 // export connection see activity 16
+const mysql = require("mysql");
+const util = require("util");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "SummerFalls13",
+    database: "cats_db"
+});
+
+connection.connect(function (error) {
+    if (error) {
+        throw error;
+    }
+
+    console.log(`Connected to database as id ${connection.threadId}`);
+});
+
+connection.query = util.promisify(connection.query);
+
+module.exports = connection;
--- models/burger.js
+++ models/burger.js
@@ -2,3 +2,34 @@
 // all functions call the orm functions
 // const cat = { all: async ()=>{orm.function call in here}} and so on
 // import the orm file here
+const orm = require("../config/orm.js");
+
+const cat = {
+  all: async () => {
+    const result = await orm.all("cat");
+
+    return result;
+  },
+
+  // The variables cols and vals are arrays.
+  create: async (cols, vals) => {
+    const result = await orm.create("cat", cols, vals);
+
+    return result;
+  },
+
+  update: async (objColVals, condition) => {
+    const result = await orm.update("cat", objColVals, condition);
+
+    return result;
+  },
+
+  delete: async (condition) => {
+    const result = await orm.delete("cat", condition);
+
+    return result;
+  }
+};
+
+// Export the database functions for the controller (catsController.js).
+module.exports = cat;
