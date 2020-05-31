0049ba3c2561210f1f30bebdb8873c3d1774beab
Nathan Darnell <darnell.nathaniel95@gmail.com>
2020-05-31 15:58:04 -0700

    seeds.sql updated to have true and false values, mmain.handlebars css file source updated, eat-block handlebar updated to say burger for now, index-handlebar updated to have burger info instead of cat info

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,6 @@
 USE burgers_db;
 
-INSERT INTO burgers (burger_name) VALUES ('Double Cheeseburger');
+INSERT INTO burgers(burger_name, devoured) VALUES ('Avacado Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheeseburger', true);
 INSERT INTO burgers (burger_name) VALUES ('Western Bacon Cheeseburger');
 INSERT INTO burgers (burger_name) VALUES ('Imossible burger');
